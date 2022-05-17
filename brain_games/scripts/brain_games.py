09f17f16bef8a797cfee4d099e09aec6eaaeacb9
Olga <jasminess847@gmail.com>
2022-05-17 22:42:22 +0300

    add linter

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -3,8 +3,10 @@ from brain_games.cli import welcome_user
 
 
 def main():
-	print("Welcome to the Brain Games!")
-	welcome_user()
-if __name__ == '__main__':
-	main()
+    print("Welcome to the Brain Games!")
 
+    welcome_user()
+
+
+if __name__ == '__main__':
+    main()
